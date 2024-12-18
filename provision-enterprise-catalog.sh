d16b8da604d3742dd1e8067b5ecade6e400f30e4
Hunia Fatima <hunia.fatima@arbisoft.com>
2024-12-18 18:49:42 +0500

    chore: add requirements installation

--- provision-enterprise-catalog.sh
+++ provision-enterprise-catalog.sh
@@ -7,6 +7,9 @@ docker compose up -d $name
 echo -e "${GREEN}Running migrations for ${name}...${NC}"
 docker compose exec -T ${name} bash -c "cd /edx/app/${name}/${name}/ && make migrate"
 
+echo -e "${GREEN}Installing requirements for ${name}...${NC}"
+docker compose exec -T ${name}  bash -e -c 'cd /edx/app/enterprise-catalog/ && make requirements' -- "$name"
+
 # Create superuser
 echo -e "${GREEN}Creating super-user for ${name}...${NC}"
 docker compose exec -T ${name} bash -c "echo 'from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\"edx\", \"edx@example.com\", \"edx\") if not User.objects.filter(username=\"edx\").exists() else None' | python /edx/app/${name}/${name}/manage.py shell"
